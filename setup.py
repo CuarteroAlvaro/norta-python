8d161b6d066441f29b641a56b697b22b3d2d341f
CuarteroAlvaro <alvaro.cuartero@komorebi.ai>
2022-05-06 13:59:52 +0200

    Update setup and test script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="norta",
+    version="0.1.0",
+    author="CuarteroAlvaro",
+    url="hhttps://github.com/CuarteroAlvaro/norta-python.git",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    setup_requires=["setuptools_scm"],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "statistics", 
+        "tqdm"
+    ],
+)
---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from norta.norta import Norta 
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    data = np.zeros((10000, 2))
+    data[:, 0] = np.random.normal(size = data.shape[0], loc = 0, scale = 17)
+    data[:, 1] = np.random.normal(size = data.shape[0], loc = 6, scale = 1)
+
+    norta = Norta(data)
+    samples = norta.generate_samples(n_samples=10000, n_bins = 150)
+
+    for i in range(data.shape[1]):
+        df = pd.DataFrame({'original':  data[:, i], 'generated': samples[:, i]})
+
+        df['original'].hist(bins = 100, density = 1, legend = "original")
+        df['generated'].hist(bins = 100, density = 1, legend = "generated")
+
+        plt.show()
