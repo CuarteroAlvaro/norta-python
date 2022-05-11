486ce3eb15708b773d494a196a6d4b4585719018
Álvaro Cuartero <87264220+CuarteroAlvaro@users.noreply.github.com>
2022-05-11 12:05:32 +0200

    Update setup.py
    Add classifiers and keywords

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="norta",
+    name="norta-python",
     version="0.1.0",
     author="CuarteroAlvaro",
     url="hhttps://github.com/CuarteroAlvaro/norta-python.git",
     packages=find_packages(),
     python_requires=">=3.9",
     setup_requires=["setuptools_scm"],
+    keywords=["statistics", "norta"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
     install_requires=[
         "numpy",
         "scipy",
